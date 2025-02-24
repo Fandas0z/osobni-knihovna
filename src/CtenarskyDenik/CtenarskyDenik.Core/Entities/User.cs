9f2e3197f3a795f372ccd7181575f7cb380939b9
František Smelík <SmelikFrantisek@litv.sssvt.cz>
2025-02-24 09:32:15 +0100

    backend class User

---
+++ src/CtenarskyDenik/CtenarskyDenik.Core/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtenarskyDenik.Core.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+        public ICollection<Note> Notes { get; set; } = new List<Note>();
+    }
+}
